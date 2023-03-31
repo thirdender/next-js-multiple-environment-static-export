ce624058d33d51aaec902243ce83dd6199809b35
Robert Rothermel <thirdender@gmail.com>
2023-03-31 17:22:18 -0400

    Consistent manifest URLs between environments
    @see https://stackoverflow.com/a/72455225/379160

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const { execSync } = require('child_process')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  generateBuildId: async () => execSync('git rev-parse HEAD').toString().trim(),
 }
 
 module.exports = nextConfig
